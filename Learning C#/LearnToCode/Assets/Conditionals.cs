d0518381c9cf34430ffbaf3e2a6ac1a35582d0f6
Yerania Hernandez <Yerania Hernandez>
2017-09-19 22:28:32 -0500

    Learning to use C# with unity

---
+++ Learning C#/LearnToCode/Assets/Conditionals.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour {
+    int health = -20;
+    bool gameOver = false;
+	// Use this for initialization
+	void Start () {
+		if (health <= 0) {
+            gameOver = true;
+        }
+        if(gameOver == true)
+        {
+            Debug.Log("Game OVER!");
+        }
+        else
+        {
+            Debug.Log("You alive!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
