918f52d9cfb8d6d8b0f6a5853e86a516a87f2790
Yerania Hernandez <Yerania Hernandez>
2017-09-23 13:07:01 -0500

    Studying OO properties

--- Learning C#/Arrays/Assets/Humanoid.cs
+++ Learning C#/Arrays/Assets/Humanoid.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Humanoid : MonoBehaviour {
+    protected int health;
+    protected int attackDamage;
+    protected float movementSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public virtual void TakeDamage(int damageToTake) {
+        health -= damageToTake;
+    }
+    public virtual int Attack() {
+        return attackDamage;
+    }
+    public virtual void Move() {
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+    public void Die() {
+
+    }
 }
